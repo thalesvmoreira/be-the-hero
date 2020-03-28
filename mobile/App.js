c470981d0903d475b27d6a955868c1cd74456d65
Thales <thalesvmoreira@hotmail.com>
2020-03-27 22:53:49 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,15 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+// WEB: <div> <span> <p> <h1>
+
+import Routes from './src/routes';
+
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
